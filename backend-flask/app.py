90e2d6d892e29c24b5b801162ec4836d4d0e3513
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-03-01 22:28:11 +0000

    add xray dependency and create resources

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,14 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#X-Ray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 # HoneyComb
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -28,6 +36,11 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+#X-Ray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 
