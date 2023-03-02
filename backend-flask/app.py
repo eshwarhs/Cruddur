dc9d0af296cbd805ca88e6662cb9acdc780ead33
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-03-02 23:45:32 +0000

    fix error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -145,7 +145,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 #@xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run(logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
