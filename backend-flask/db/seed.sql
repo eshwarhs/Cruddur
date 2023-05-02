442a2463cb99ea06284a0977227f31ad2e1779c9
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-05-02 00:17:16 +0000

    fix messages short return type

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Eshwar H S', 'eshwarhs99@gmail.com', 'eshwarhs' ,'MOCK'),
-  ('Eshwar Hebbur', 'eshwar1011236@gmail.com', 'eshwarhebbur' ,'MOCK');;
+  ('Eshwar Hebbur', 'eshwar1011236@gmail.com', 'eshwarhebbur' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
