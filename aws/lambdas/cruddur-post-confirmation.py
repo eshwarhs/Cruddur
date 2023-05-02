f21502d6e5bc7b17f8d30320a69f54da387d0cff
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-05-02 00:36:10 +0000

    fix messaging

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -32,7 +32,7 @@ def lambda_handler(event, context):
         user_handle,
         user_cognito_id
       ]
-      cur.execute(sql,*params)
+      cur.execute(sql,(user_display_name, user_email, user_handle, user_cognito_id))
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
