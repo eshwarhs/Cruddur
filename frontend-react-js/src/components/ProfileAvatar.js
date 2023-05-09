579ca2fe8fb2cd6243fd2c0b6cf5f05f79538ae5
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-05-09 05:04:46 +0000

    render profile pic in activities

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -3,11 +3,6 @@ import './ProfileAvatar.css';
 export default function ProfileAvatar(props) {
   const backgroundImage = `url("https://assets.crudders.click/avatars/${props.id}.jpg")`;
   const styles = {
-    // background: 'rgb(149,0,255)',
-    // height: '40px',
-    // width: '40px',
-    // 'border-radius': '999px',
-    // 'flex-shrink': 0,
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
