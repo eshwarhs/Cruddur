7153730686d52aefc4c1cfd2cff490779b16b061
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-07-20 19:46:13 +0000

    Fix suggested users hardcoded image

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -13,7 +13,7 @@ export default function DesktopSidebar(props) {
   ]
 
   const users = [
-    {"display_name": "Andrew Brown", "handle": "andrewbrown"}
+    {"display_name": "Andrew Brown", "handle": "andrewbrown", "id": 1234}
   ]
 
   let trending;
--- frontend-react-js/src/components/SuggestedUserItem.js
+++ frontend-react-js/src/components/SuggestedUserItem.js
@@ -1,9 +1,22 @@
 import './SuggestedUserItem.css';
+import ProfileAvatar from 'components/ProfileAvatar'
 
 export default function SugestedUserItem(props) {
+  const backgroundImage = `url("https://assets.crudders.click/avatars/${props.id}.jpg")`;
+  const styles = {
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minWidth: '36px',
+    minHeight: '36px',
+    borderRadius: '999px'
+  };
   return (
     <div className="user">
-      <div className='avatar'></div>
+      <div className='avatar'><div
+        className="suggested-avatar"
+        style={styles}
+      ></div></div>
       <div className='identity'>
         <span className="display_name">{props.display_name}</span>
         <span className="handle">@{props.handle}</span>
