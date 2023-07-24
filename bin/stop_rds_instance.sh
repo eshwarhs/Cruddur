3993ed5d2933886b51cff446186033c17c9ef2ea
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-07-24 14:23:58 -0700

    Update stop_rds_instance.sh

--- bin/stop_rds_instance.sh
+++ bin/stop_rds_instance.sh
@@ -3,5 +3,14 @@
 # Replace 'YOUR_RDS_INSTANCE_IDENTIFIER' with the actual RDS instance identifier.
 RDS_INSTANCE_IDENTIFIER="cruddur-instance"
 
-# Stop the RDS instance
-aws rds stop-db-instance --db-instance-identifier $RDS_INSTANCE_IDENTIFIER
+# Check the status of the RDS instance
+RDS_STATUS=$(aws rds describe-db-instances --db-instance-identifier $RDS_INSTANCE_IDENTIFIER --query 'DBInstances[0].DBInstanceStatus' --output text)
+
+if [ "$RDS_STATUS" == "available" ]; then
+  # Stop the RDS instance if it's running
+  aws rds stop-db-instance --db-instance-identifier $RDS_INSTANCE_IDENTIFIER
+  echo "AWS RDS instance is stopped."
+else
+  echo "AWS RDS instance is not running. No action needed."
+fi
+
