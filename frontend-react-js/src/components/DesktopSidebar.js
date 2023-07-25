590fe8f4d3d83e0e14040d9528936eb8d9700234
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-07-25 18:27:49 +0000

    add assets:

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -13,7 +13,7 @@ export default function DesktopSidebar(props) {
   ]
 
   const users = [
-    {"display_name": "Andrew Brown", "handle": "andrewbrown", "id": 1234}
+    {"display_name": "Eshwar Hebbur Shivakumar", "handle": "eshwarhs", "id": 1234}
   ]
 
   let trending;
